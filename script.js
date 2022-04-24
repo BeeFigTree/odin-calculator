9e3eb58a49a992d01e737e2dde37fcca257ff3c7
Brian Figueroa <scissors520@gmail.com>
2022-04-24 16:26:08 -0700

    Start functionality

--- script.js
+++ script.js
@@ -1,7 +1,34 @@
 // Variables
+const calculatorDisplay = document.querySelector(".calculator__display");
+const calculatorNumberButtons = Array.from(document.querySelectorAll(".button--number"));
+const calculatorOperations = Array.from(document.querySelectorAll('.operation'));
+
+class Calculator {
+    constructor(previousValue, currentValue, currentOperation) {
+        this.previousValue = previousValue;
+        this.currentValue = currentValue;
+        this.currentOperation = currentOperation;
+    }
+}
+
+const calculator = new Calculator(null, 0, null);
+console.log(calculator)
+
+const operationMap = {
+    "%": "",
+    "CE": "",
+    "C": "",
+    "/": "",
+    "*": "",
+    "-": "",
+    "+": "",
+}
 
 // Util functions
 
+
 // Math functions
+
+
 
 // Event listeners
