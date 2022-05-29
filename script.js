c2aab9ebf298d7fe743e0bfe8227dc2b8370a1ee
Brian Figueroa <brian@Brians-MacBook-Pro.local>
2022-05-28 22:31:18 -0700

    Huh

--- script.js
+++ script.js
@@ -1,41 +1,87 @@
 // Variables
 const calculatorDisplay = document.querySelector(".calculator__display");
 const calculatorNumberButtons = Array.from(document.querySelectorAll(".button--number"));
-const calculatorOperations = Array.from(document.querySelectorAll('.operation'));
-
-const operationMap = {
-    "%": "",
-    "CE": "",
-    "C": "",
-    "/": "",
-    "*": "",
-    "-": "",
-    "+": "",
-}
+const calculatorOperations = Array.from(document.querySelectorAll(".operation"));
 
 class Calculator {
     constructor(previousValue, currentValue, currentOperation) {
         this.previousValue = previousValue;
         this.currentValue = currentValue;
         this.currentOperation = currentOperation;
+        this.operationMap = {
+            "%": this.percentage,
+            "CE": this.clearCurrentValue,
+            "C": this.clearAll,
+            "/": this.division,
+            "*": this.multiplication,
+            "-": this.subtraction,
+            "+": this.addition,
+        }
     }
+
+    updateDisplay = (event) => {
+        if(!event) {
+            calculatorDisplay.innerHTML = this.previousValue + " " + this.currentOperation;
+            calculatorDisplay.value = undefined;
+            return;
+        }
+
+        if(calculatorDisplay.value) {
+            calculatorDisplay.value = calculatorDisplay.value + event.target.value;
+        } else {
+            calculatorDisplay.value = event.target.value;
+        }
+        if(!this.currentOperation) {
+            calculatorDisplay.innerHTML = calculatorDisplay.value;
+        } else {
+            calculatorDisplay.innerHTML = calculatorDisplay.innerHTML + " " + calculatorDisplay.value;
+        }
+    }
+
+    setOperation = (operation) => {
+        this.currentOperation = operation.target.value;
+        this.setPreviousValue(this.getCurrentValue());
+        this.updateDisplay();
+        console.log(this.previousValue, this.currentOperation)
+    }
+    
+    setPreviousValue = (currentValue) => {
+        this.previousValue = currentValue;
+    }
+    
+    getCurrentValue = () => {
+        return calculatorDisplay.value;
+    }
+
+    addition = () => {
+        return this.previousValue + this.currentValue;
+    }
+    
+    subtraction = () => {
+        return this.previousValue - this.currentValue;
+    }
+    
+    division = () => {
+        return this.previousValue / this.currentValue;
+    }
+    
+    multiplication = () => {
+        return this.previousValue * this.currentValue;
+    }
+    
+    percentage = () => {
+        return (this.previousValue / 100) * this.currentValue;
+    }
+
 }
 
 const calculator = new Calculator(null, 0, null);
 
-// Util functions
-const setOperation = (operation) => {
-    calculator.currentOperation = operation.target.value;
-}
-
-const setPreviousValue = () => {
-    calculator.previousValue = currentValue;
-}
-
-// Math functions
-
-
 // Event listeners
 calculatorOperations.map(operation => {
-    operation.addEventListener("click", setOperation)
-})
+    operation.addEventListener("click", calculator.setOperation)
+});
+
+calculatorNumberButtons.map(number => {
+    number.addEventListener("click", calculator.updateDisplay)
+});
